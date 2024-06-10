f99e897c15874293d3e417adcd4a691ab131873a
nilomr <nilomerinorecalde@gmail.com>
2024-06-10 13:17:28 +0100

    Add script to download and unzip metadata data

---
+++ scripts/wget-data.py
@@ -0,0 +1,20 @@
+import subprocess
+from pathlib import Path
+
+from majorvocal.config import config
+
+folder_names = [path for path in Path(config.DATA_PATH).iterdir() if (path.is_dir() and "GRETI_20" in path.name)]
+
+
+# Wytham Great Tit song metadata - download and unzip
+folder_url = "https://files.de-1.osf.io/v1/resources/n8ac9/providers/osfstorage/64898c4bc861160288251fd1/?zip"
+save_path = config.PROJECT_STRUCTURE["metadata"] / "metadata.zip"
+
+subprocess.run(["wget", "--show-progress", "--progress=bar:force 2>&1", "-O", str(save_path), folder_url])
+subprocess.run(["unzip", str(save_path), "-d", str(save_path.parent)])
+
+# Download first song times (manual annotations)
+times_url = "https://raw.githubusercontent.com/nilomr/great-tit-hits-setup/main/resources/birds/times.csv"
+save_times_path = config.PROJECT_STRUCTURE["metadata"] / "times.csv"
+
+subprocess.run(["wget", "--show-progress", "--progress=bar:force 2>&1", "-O", str(save_times_path), times_url])
